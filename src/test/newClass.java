a7ba079561072de047685916ca038d9b432ed5fe
HP <HP@DESKTOP-R4NKMTL>
2024-06-04 22:31:05 +0530

    4th commit

--- src/test/newClass.java
+++ src/test/newClass.java
@@ -4,8 +4,8 @@ public class newClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello1");
-		System.out.print("Hello2");
+		System.out.print("Hello11");
+		System.out.print("Hello21");
 		
 
 	}
